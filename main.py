29bdf386abba4739d19615580444631cc17d311a
Tim Tkachenko <wassertim@gmail.com>
2024-04-26 14:49:30 +0000

    use neutral message

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from lib.image_editor import create_cover_image
 
 def main():    
     create_cover_image(
-        file_path='./original.jpeg',
-        title='LINDAU IN LAKE CONSTANCE',
-        subtitle='WALKING TO THE BAVARIAN LION'
+        file_path='./original.jpeg'
     )
 
 if __name__ == "__main__":
